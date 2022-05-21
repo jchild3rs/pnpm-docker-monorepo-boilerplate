3bae7c5e68b43e546a0a7a7c64cf3622f7b85312
James Childers <jchilders@rentpath.com>
2022-05-21 16:14:17 -0400

    i guess this is what i want

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,10 @@
+const path = require("path");
 const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 });
