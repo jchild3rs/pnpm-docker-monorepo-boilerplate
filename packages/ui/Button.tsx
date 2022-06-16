929e197130ee5c7e4a0f48dbcc55532448a47ffb
James Childers <jchilders62@gmail.com>
2022-06-16 18:55:28 -0400

    change to ui (should trigger only web to change)

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 
-export const Button = () => {
+export const Button = ({ children }: { children?: React.ReactNode }) => {
   return (
     <button className="text-red-900 font-bold text-3xl">
-      Boop!!
+      {children}
     </button>
   )
 }
