27f6890deab2650de22c95d0b90c0a0e80e652af
James Childers <jchilders62@gmail.com>
2022-06-25 12:08:34 -0400

    chore: add prettier and format

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
 
 export const Button = ({ children }: { children?: React.ReactNode }) => {
-  return (
-    <button className="text-red-900 font-bold text-3xl">
-      {children}
-    </button>
-  )
+  return <button className="text-red-900 font-bold text-3xl">{children}</button>
 }
