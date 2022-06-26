f1bd8aad0a778e880a031bc3a251e71937318a8e
James Childers <jchilders62@gmail.com>
2022-06-26 15:06:42 -0400

    update storybook logo to match fake web app

--- apps/docs/.storybook/manager.js
+++ apps/docs/.storybook/manager.js
@@ -2,5 +2,8 @@ import { addons } from '@storybook/addons'
 import { themes } from '@storybook/theming'
 
 addons.setConfig({
-  theme: themes.dark
+  theme: {
+    ...themes.dark,
+    brandImage: 'https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
+  }
 })
